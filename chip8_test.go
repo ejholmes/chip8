6ddf1f9515be71cbbad741aa67c8f6926b1f01ea
Eric J. Holmes <eric@ejholmes.net>
2014-10-02 08:32:52 -0700

    Cleanup tests.

--- chip8_test.go
+++ chip8_test.go
@@ -2,6 +2,330 @@ package chip8
 
 import "testing"
 
+var opcodeTests = map[string][]struct {
+	op     uint16
+	before func(*testing.T, *CPU)
+	check  func(*testing.T, *CPU)
+}{
+	"2nnn - CALL addr": {
+		{
+			uint16(0x2100),
+			nil,
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "Stack[0]", c.Stack[0], uint16(0xC8))
+				checkHex(t, "SP", uint16(c.SP), uint16(0x1))
+				checkHex(t, "PC", c.PC, uint16(0x100))
+			},
+		},
+	},
+
+	"3xkk - SE Vx, byte": {
+		{
+			uint16(0x3123),
+			nil,
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "PC", c.PC, uint16(200))
+			},
+		},
+
+		{
+			uint16(0x3103),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x03
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "PC", c.PC, uint16(202))
+			},
+		},
+	},
+
+	"4xkk - SNE Vx, byte": {
+		{
+			uint16(0x4123),
+			nil,
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "PC", c.PC, uint16(202))
+			},
+		},
+
+		{
+			uint16(0x4103),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x03
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "PC", c.PC, uint16(200))
+			},
+		},
+	},
+
+	"5xy0 - SE Vx, Vy": {
+		{
+			uint16(0x5120),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x03
+				c.V[2] = 0x04
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "PC", c.PC, uint16(200))
+			},
+		},
+
+		{
+			uint16(0x5120),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x03
+				c.V[2] = 0x03
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "PC", c.PC, uint16(202))
+			},
+		},
+	},
+
+	"6xkk - LD Vx, byte": {
+		{
+			uint16(0x6102),
+			nil,
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x02))
+			},
+		},
+	},
+
+	"7xkk - ADD Vx, byte": {
+		{
+			uint16(0x7102),
+			nil,
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x02))
+			},
+		},
+
+		{
+			uint16(0x7102),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x01
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x03))
+			},
+		},
+	},
+
+	"8xy0 - LD Vx, Vy": {
+		{
+			uint16(0x8120),
+			func(t *testing.T, c *CPU) {
+				c.V[2] = 0x01
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x01))
+			},
+		},
+	},
+
+	"8xy1 - OR Vx, Vy": {
+		{
+			uint16(0x8121),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x10
+				c.V[2] = 0x01
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x11))
+			},
+		},
+	},
+
+	"8xy2 - AND Vx, Vy": {
+		{
+			uint16(0x8122),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x10
+				c.V[2] = 0x01
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x00))
+			},
+		},
+	},
+
+	// TODO
+	"Set Vx = Vx AND Vy": {
+		{
+			uint16(0x8123),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x01
+				c.V[2] = 0x01
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x00))
+			},
+		},
+	},
+
+	"8xy4 - ADD Vx, Vy": {
+		{
+			uint16(0x8124),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x01
+				c.V[2] = 0x01
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x2))
+				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x0))
+			},
+		},
+
+		{
+			uint16(0x8124),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0xFF
+				c.V[2] = 0x03
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x2))
+				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x1))
+			},
+		},
+	},
+
+	"8xy5 - SUB Vx, Vy": {
+		{
+			uint16(0x8125),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0xFF
+				c.V[2] = 0x03
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x1))
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0xFC))
+			},
+		},
+
+		{
+			uint16(0x8125),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x02
+				c.V[2] = 0x03
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x0))
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0xFF))
+			},
+		},
+	},
+
+	"8xy6 - SHR Vx {, Vy}": {
+		{
+			uint16(0x8126),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x03
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x1))
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x1))
+			},
+		},
+
+		{
+			uint16(0x8126),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x02
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x0))
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x1))
+			},
+		},
+	},
+
+	"8xy7 - SUBN Vx, Vy": {
+		{
+			uint16(0x8127),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x03
+				c.V[2] = 0xFF
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x1))
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0xFC))
+			},
+		},
+
+		{
+			uint16(0x8127),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x03
+				c.V[2] = 0x02
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x0))
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0xFF))
+			},
+		},
+	},
+
+	"8xyE - SHL Vx {, Vy}": {
+		{
+			uint16(0x812E),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x01
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x0))
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x2))
+			},
+		},
+
+		{
+			uint16(0x812E),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x81
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x1))
+				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x2))
+			},
+		},
+	},
+
+	"9xy0 - SNE Vx, Vy": {
+		{
+			uint16(0x9120),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x01
+				c.V[2] = 0x02
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "PC", c.PC, uint16(202))
+			},
+		},
+
+		{
+			uint16(0x9120),
+			func(t *testing.T, c *CPU) {
+				c.V[1] = 0x01
+				c.V[2] = 0x01
+			},
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "PC", c.PC, uint16(200))
+			},
+		},
+	},
+
+	"Annn - LD I, addr": {
+		{
+			uint16(0xA100),
+			nil,
+			func(t *testing.T, c *CPU) {
+				checkHex(t, "I", c.I, uint16(0x100))
+			},
+		},
+	},
+}
+
 func checkHex(t *testing.T, subject string, got, want uint16) {
 	if got != want {
 		t.Errorf("%s => 0x%04X; want 0x%04X", subject, got, want)
@@ -20,310 +344,24 @@ func TestCPU_Step(t *testing.T) {
 	checkHex(t, "PC", c.PC, uint16(202))
 }
 
-func TestCPU_Dispatch(t *testing.T) {
-	tests := []struct {
-		op     uint16
-		before func(*CPU)
-		check  func(*CPU)
-	}{
-		{
-			uint16(0x2100),
-			nil,
-			func(c *CPU) {
-				checkHex(t, "Stack[0]", c.Stack[0], uint16(0xC8))
-				checkHex(t, "SP", uint16(c.SP), uint16(0x1))
-				checkHex(t, "PC", c.PC, uint16(0x100))
-			},
-		},
+func TestOpcodes(t *testing.T) {
+	for i, tests := range opcodeTests {
+		for _, tt := range tests {
+			c := NewCPU(nil)
+			if tt.before != nil {
+				tt.before(t, c)
+			}
+			c.Dispatch(tt.op)
+			tt.check(t, c)
 
-		{
-			uint16(0x3123),
-			nil,
-			func(c *CPU) {
-				checkHex(t, "PC", c.PC, uint16(200))
-			},
-		},
-
-		{
-			uint16(0x3103),
-			func(c *CPU) {
-				c.V[1] = 0x03
-			},
-			func(c *CPU) {
-				checkHex(t, "PC", c.PC, uint16(202))
-			},
-		},
-
-		{
-			uint16(0x4123),
-			nil,
-			func(c *CPU) {
-				checkHex(t, "PC", c.PC, uint16(202))
-			},
-		},
-
-		{
-			uint16(0x4103),
-			func(c *CPU) {
-				c.V[1] = 0x03
-			},
-			func(c *CPU) {
-				checkHex(t, "PC", c.PC, uint16(200))
-			},
-		},
-
-		{
-			uint16(0x5120),
-			func(c *CPU) {
-				c.V[1] = 0x03
-				c.V[2] = 0x04
-			},
-			func(c *CPU) {
-				checkHex(t, "PC", c.PC, uint16(200))
-			},
-		},
-
-		{
-			uint16(0x5120),
-			func(c *CPU) {
-				c.V[1] = 0x03
-				c.V[2] = 0x03
-			},
-			func(c *CPU) {
-				checkHex(t, "PC", c.PC, uint16(202))
-			},
-		},
-
-		{
-			uint16(0x6102),
-			nil,
-			func(c *CPU) {
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x02))
-			},
-		},
-
-		{
-			uint16(0x7102),
-			nil,
-			func(c *CPU) {
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x02))
-			},
-		},
-
-		{
-			uint16(0x7102),
-			func(c *CPU) {
-				c.V[1] = 0x01
-			},
-			func(c *CPU) {
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x03))
-			},
-		},
-
-		{
-			uint16(0x8120),
-			func(c *CPU) {
-				c.V[2] = 0x01
-			},
-			func(c *CPU) {
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x01))
-			},
-		},
-
-		{
-			uint16(0x8121),
-			func(c *CPU) {
-				c.V[1] = 0x10
-				c.V[2] = 0x01
-			},
-			func(c *CPU) {
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x11))
-			},
-		},
-
-		{
-			uint16(0x8122),
-			func(c *CPU) {
-				c.V[1] = 0x10
-				c.V[2] = 0x01
-			},
-			func(c *CPU) {
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x00))
-			},
-		},
-
-		{
-			uint16(0x8123),
-			func(c *CPU) {
-				c.V[1] = 0x01
-				c.V[2] = 0x01
-			},
-			func(c *CPU) {
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x00))
-			},
-		},
-
-		{
-			uint16(0x8124),
-			func(c *CPU) {
-				c.V[1] = 0x01
-				c.V[2] = 0x01
-			},
-			func(c *CPU) {
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x2))
-				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x0))
-			},
-		},
-
-		{
-			uint16(0x8124),
-			func(c *CPU) {
-				c.V[1] = 0xFF
-				c.V[2] = 0x03
-			},
-			func(c *CPU) {
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x2))
-				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x1))
-			},
-		},
-
-		{
-			uint16(0x8125),
-			func(c *CPU) {
-				c.V[1] = 0xFF
-				c.V[2] = 0x03
-			},
-			func(c *CPU) {
-				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x1))
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0xFC))
-			},
-		},
-
-		{
-			uint16(0x8125),
-			func(c *CPU) {
-				c.V[1] = 0x02
-				c.V[2] = 0x03
-			},
-			func(c *CPU) {
-				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x0))
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0xFF))
-			},
-		},
-
-		{
-			uint16(0x8126),
-			func(c *CPU) {
-				c.V[1] = 0x03
-			},
-			func(c *CPU) {
-				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x1))
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x1))
-			},
-		},
-
-		{
-			uint16(0x8126),
-			func(c *CPU) {
-				c.V[1] = 0x02
-			},
-			func(c *CPU) {
-				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x0))
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x1))
-			},
-		},
-
-		{
-			uint16(0x8127),
-			func(c *CPU) {
-				c.V[1] = 0x03
-				c.V[2] = 0xFF
-			},
-			func(c *CPU) {
-				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x1))
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0xFC))
-			},
-		},
-
-		{
-			uint16(0x8127),
-			func(c *CPU) {
-				c.V[1] = 0x03
-				c.V[2] = 0x02
-			},
-			func(c *CPU) {
-				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x0))
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0xFF))
-			},
-		},
-
-		{
-			uint16(0x812E),
-			func(c *CPU) {
-				c.V[1] = 0x01
-			},
-			func(c *CPU) {
-				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x0))
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x2))
-			},
-		},
-
-		{
-			uint16(0x812E),
-			func(c *CPU) {
-				c.V[1] = 0x81
-			},
-			func(c *CPU) {
-				checkHex(t, "VF", uint16(c.V[0xF]), uint16(0x1))
-				checkHex(t, "V[1]", uint16(c.V[1]), uint16(0x2))
-			},
-		},
-
-		{
-			uint16(0x9120),
-			func(c *CPU) {
-				c.V[1] = 0x01
-				c.V[2] = 0x02
-			},
-			func(c *CPU) {
-				checkHex(t, "PC", c.PC, uint16(202))
-			},
-		},
-
-		{
-			uint16(0x9120),
-			func(c *CPU) {
-				c.V[1] = 0x01
-				c.V[2] = 0x01
-			},
-			func(c *CPU) {
-				checkHex(t, "PC", c.PC, uint16(200))
-			},
-		},
-
-		{
-			uint16(0xA100),
-			nil,
-			func(c *CPU) {
-				checkHex(t, "I", c.I, uint16(0x100))
-			},
-		},
-	}
-
-	for _, tt := range tests {
-		c := NewCPU(nil)
-		if tt.before != nil {
-			tt.before(c)
-		}
-		c.Dispatch(tt.op)
-		tt.check(c)
-
-		if t.Failed() {
-			t.Logf("==============")
-			t.Logf("Opcode: 0x%04X", tt.op)
-			t.Logf("CPU: %v", c)
-			t.Logf("==============")
-			t.FailNow()
+			if t.Failed() {
+				t.Logf("==============")
+				t.Logf("Instruction: %s", i)
+				t.Logf("Opcode: 0x%04X", tt.op)
+				t.Logf("CPU: %v", c)
+				t.Logf("==============")
+				t.FailNow()
+			}
 		}
 	}
 }
