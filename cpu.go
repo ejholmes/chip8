26109eff65fc7b381eac0633ccf4d3552a72c45a
Eric J. Holmes <eric@ejholmes.net>
2014-10-05 07:44:29 -0700

    Package doc.

--- cpu.go
+++ cpu.go
@@ -4,17 +4,13 @@
 
 // Package chip8 provides a Go implementation of the CHIP-8 emulator.
 //
-// CHIP-8 was most commonly implemented on 4K systems, such as the
-// Cosmac VIP and the Telemac 1800. These machines had 4096 (0x1000)
-// memory locations, all of which are 8 bits (a byte) which is where the
-// term CHIP-8 originated. However, the CHIP-8 interpreter itself
-// occupies the first 512 bytes of the memory space on these machines.
-// For this reason, most programs written for the original system begin
-// at memory location 512 (0x200) and do not access any of the memory
-// below the location 512 (0x200). The uppermost 256 bytes (0xF00-0xFFF)
-// are reserved for display refresh, and the 96 bytes below that
-// (0xEA0-0XEFF) were reserved for call stack, internal use, and other
-// variables.
+// CHIP-8 is an interpreted programming language, developed by Joseph
+// Weisbecker. It was initially used on the COSMAC VIP and Telmac 1800 8-bit
+// microcomputers in the mid-1970s. CHIP-8 programs are run on a CHIP-8 virtual
+// machine. It was made to allow video games to be more easily programmed for
+// said computers.
+//
+// This package provides a Go implementation that can run CHIP-8 binaries.
 package chip8
 
 import (
